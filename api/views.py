cb8bf0d739a5fa7295d9b6c891bc01cfc6088250
Kartik Dholakia <kartikdholakia5555@gmail.com>
2022-02-12 16:53:43 +0530

    created api using rest_framework

--- api/views.py
+++ api/views.py
@@ -1,40 +1,63 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
+
+# decorators:
+@api_view(['GET'])      # takes the list of http methods (get, post, put, ...)
+
 def getRoutes(request):
 	
 	routes = [
-        {
-            'Endpoint': '/notes/',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns an array of notes'
-        },
-        {
-            'Endpoint': '/notes/id',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns a single note object'
-        },
-        {
-            'Endpoint': '/notes/create/',
-            'method': 'POST',
-            'body': {'body': ""},
-            'description': 'Creates new note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/update/',
-            'method': 'PUT',
-            'body': {'body': ""},
-            'description': 'Creates an existing note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/delete/',
-            'method': 'DELETE',
-            'body': None,
-            'description': 'Deletes and exiting note'
-        },
-    ]
+		{
+			'Endpoint': '/notes/',
+			'method': 'GET',
+			'body': None,
+			'description': 'Returns an array of notes'
+		},
+		{
+			'Endpoint': '/notes/id',
+			'method': 'GET',
+			'body': None,
+			'description': 'Returns a single note object'
+		},
+		{
+			'Endpoint': '/notes/create/',
+			'method': 'POST',
+			'body': {'body': ""},
+			'description': 'Creates new note with data sent in post request'
+		},
+		{
+			'Endpoint': '/notes/id/update/',
+			'method': 'PUT',
+			'body': {'body': ""},
+			'description': 'Creates an existing note with data sent in post request'
+		},
+		{
+			'Endpoint': '/notes/id/delete/',
+			'method': 'DELETE',
+			'body': None,
+			'description': 'Deletes and exiting note'
+		},
+	]
 
-	return JsonResponse(routes, safe=False)
+	return Response(routes)
+
+# to get notes from the database
+# in order to render the python objects, we first need to serialize them
+@api_view(['GET'])
+def getNotes(request):
+	notes = Note.objects.all()  # this returns the list of python objects
+	serial_class = NoteSerializer(notes, many=True)     # many=True means that we want to serialize multiple objects
+	
+	return Response(serial_class.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+	notes = Note.objects.get(id=pk)
+	serial_class = NoteSerializer(notes, many=False)
+	
+	return Response(serial_class.data)    
